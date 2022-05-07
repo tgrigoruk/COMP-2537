a9102e91d956a605772ad40b15324f528f67f408
Terence Grigoruk <terence.grigoruk@gmail.com>
2022-05-07 14:22:38 -0700

    added profile page data logic

--- assignment1/server.js
+++ assignment1/server.js
@@ -10,22 +10,38 @@ app.listen(5001, function (err) {
 });
 
 // https://pokeapi.co/
-const pokeurl = "https://pokeapi.co/api/v2/pokemon"; // + id or name
 
 app.get("/", function (req, res) {
   res.send("<h1>GET request to homepage</h1>");
 });
 
-app.get("profile/:id", function (req, res) {
-  console.log(pokeurl);
-  https.get(pokeurl + req.params.id, function (https_res) {
-    https_res.on("data", function (data) {
-      // console.log(data);
+app.get("/profile/:id", function (req, res) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${req.params.id}`;
+  data = "";
+  https.get(url, function (https_res) {
+    https_res.on("data", function (chunk) {
+      data += chunk;
+    });
+    https_res.on("end", function () {
+      res.render("profile.ejs", extractPokemonData(data));
     });
   });
-
-  req.render("profile.ejs", {
-    id: req.params.id,
-    name: "",
-  });
 });
+
+function extractPokemonData(data) {
+  data = JSON.parse(data);
+  stats = Object.assign(
+    {},
+    ...data.stats.map((stat) => ({
+      [stat.stat.name]: stat.base_stat,
+    }))
+  );
+  pokemonData = {
+    name: data.name,
+    img: data.sprites.other["official-artwork"].front_default,
+    base_xp: data.base_experience,
+    stats: stats,
+  };
+  console.log(pokemonData);
+  return pokemonData;
+}
