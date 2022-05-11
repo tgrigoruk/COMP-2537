2ea0e4a2f64f88139e9c9a919077fab6d43514e4
Terence Grigoruk <terence.grigoruk@gmail.com>
2022-05-10 17:35:58 -0700

    created draft server.js for api_server

--- api_server/server.js
+++ api_server/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
-app.use(express.static("./public"));
-app.set("view engine", "ejs");
 
+const bodyparser = require("body-parser");
 app.use(
   bodyparser.urlencoded({
     parameterLimit: 100000,
@@ -11,45 +10,45 @@ app.use(
   })
 );
 
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/timelineDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const eventSchema = new mongoose.Schema({
-  text: String,
-  hits: Number,
-  time: String,
-});
-const eventModel = mongoose.model("timelineEvents", eventSchema);
-
-// prepend port number with:    process.env.PORT ||
-app.listen(5001, function (err) {
+// for Heroku prepend port number with:    process.env.PORT ||
+app.listen(5002, function (err) {
   if (err) console.log(err);
 });
 
 //---------- ROUTES ---------//
+let pokeapiUrl = "https://pokeapi.co/api/v2/";
 
-app.get("/timeline/getAllEvents", function (req, res) {
-  // console.log(req.body);
-  eventModel.find({}, function (err, data) {
-    if (err) {
-      // console err
-    } else {
-      console.log(data);
-    }
-    res.send(data);
+app.get("/profile/:id", function (req, res) {
+  const url = pokeapiUrl + req.params.id;
+  data = "";
+  https.get(url, function (https_res) {
+    https_res.on("data", function (chunk) {
+      data += chunk;
+    });
+    https_res.on("end", function () {
+      res.render("profile.ejs", extractPokemonData(data));
+    });
   });
 });
 
-app.get("/timeline/update/", function (req, res) {
-  eventModel.create({});
-});
-
-app.get("/timeline/increaseHits/:id", function (req, res) {
-  eventModel.udpateOne(
-    { _id: req.params.id },
-    { $inc: { hits: 1 } },
-    function () {}
+function extractPokemonData(data) {
+  data = JSON.parse(data);
+  stats = Object.assign(
+    {},
+    { base_xp: data.base_experience },
+    ...data.stats.map((stat) => ({
+      [stat.stat.name]: stat.base_stat,
+    }))
   );
-});
+  abilities = data.abilities.map((ability) => {
+    return ability.ability.name;
+  });
+  pokemonData = {
+    name: data.name[0].toUpperCase() + data.name.slice(1),
+    img: data.sprites.other["official-artwork"].front_default,
+    stats: stats,
+    abilities: abilities,
+  };
+  // console.log(pokemonData);
+  return pokemonData;
+}
