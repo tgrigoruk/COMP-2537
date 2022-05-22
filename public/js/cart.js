97616529794d1b0f7d49d3f5eab23e1d7a3f7c5d
tgrigoruk <terence.grigoruk@gmail.com>
2022-05-22 12:47:12 -0700

    Added change quantity function

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,16 +1,85 @@
-function addToCart(name, base_xp) {
-  console.log(name)
+function loadCartItems() {
+  $("#cart-items").empty();
   $.ajax({
-    url: `/cart/add/${name}/${base_xp}`,
+    url: "/cart/getCart",
     type: "GET",
-    success: (res) => {
-      console.log({ res })
+    success: (cart) => {
+      console.log({ cart });
+      if (cart) {
+        for (i = 0; i < cart.length; i++) {
+          parity = i % 2 ? "odd" : "even";
+          let name = cart[i].name;
+          $("#cart-items").prepend(
+            `
+            <div class="cart-item row-${parity}" id="${name}"> 
+              ${capitalize(name)}
+              <span class="cart-item-price">$${cart[i].price}</span>  
+              <div class="quantity-container">
+                <button class="quantity-button" onclick="changeQuantity('${name}', -1)">-</button>
+                <span id="${name}-quantity" class="event-hit-quantity">${cart[i].quantity}</span>            
+                <button class="quantity-button" onclick="changeQuantity('${name}', 1)">+</button>
+                <button class="delete-button" onclick=removeFromCart('${name}')>remove</button>
+              </div>
+            </div>
+            `
+          );
+        }
+        $("#cart-items").prepend(`<div class="cart-item cart-header">
+          <p>Pokemon Name</p><p>Price</p><p style="text-align:center">Quantity</p></div>`);
+      } else {
+        $("#cart-items").append(`<p id="cart-empty">Cart is currently empty</p>`);
+      }
     },
   });
 }
 
-function setup() {
-  // $(".add-button").on("click", addToBasket);
+function addToCart(name, base_xp) {
+  // console.log(name)
+  $.ajax({
+    url: `/cart/add/${name}/${base_xp}`,
+    type: "GET",
+    success: (res) => {
+      console.log({ res });
+    },
+  });
 }
 
+function changeQuantity(name, amount) {
+  console.log(name);
+  $.ajax({
+    url: `/cart/increment/${name}/${amount}`,
+    type: "GET",
+    success: (res) => {
+      if (res) {
+        $(`#${name}-quantity`).text(res.quantity);
+      }
+    },
+  });
+}
+
+
+function removeFromCart(name) {
+  // remove item from cart
+}
+
+
+function emptyCart() {
+  // empty entire cart
+
+}
+
+function proceedToCheckout() {
+  // go to screen showing totals etc.
+}
+
+function finalizeOrder() {
+  // complete order, add to orderHistory[] and clear cart
+}
+
+function capitalize(text) {
+  return text[0].toUpperCase() + text.slice(1);
+}
+function setup() {
+  loadCartItems();
+}
 $(document).ready(setup);
--- src/cart.js
+++ src/cart.js
@@ -1,6 +1,7 @@
 const express = require("express");
 var router = express.Router();
 const session = require("express-session");
+const res = require("express/lib/response");
 
 //-------------------- MONGOOSE SETUP --------------------//
 
@@ -34,65 +35,105 @@ const cartModel = mongoose.model("shoppingcarts", cartSchema);
 
 //-------------------- SHOPPING CART ROUTES --------------------//
 
-username = 'test'
+username = 'test';
 
 router.get("/add/:name/:price", function (req, res) {
-  let username = "test"
+  let username = "test";
   // const {username} = req.session
   const itemName = req.params.name;
-  const itemPrice = parseInt(req.params.price)
-  console.log(`server.js route - name: ${itemName} , price: ${itemPrice}`)
+  const itemPrice = parseInt(req.params.price);
+  console.log(`server.js route - name: ${itemName} , price: ${itemPrice}`);
 
   cartModel.find({ username: username, "cart.name": itemName }, function (err, result) {
-    console.log({ result });
+    // console.log({ result });
     if (err) {
-      printError(err)
+      printError(err);
     } else {
       if (result.length) {
-        log({ result })
-        incrementItemQuantity(username, itemName)
+        log({ result });
+        incrementItemQuantity(username, itemName, 1);
       } else {
-        addNewItemToCart(username, itemName, itemPrice)
+        addNewItemToCart(username, itemName, itemPrice);
       }
     }
-  })
+  });
 });
-function incrementItemQuantity(username, itemName) {
+function incrementItemQuantity(username, itemName, amount) {
+  let incrementResult = false;
   cartModel.updateOne(
     { username: username, "cart.name": itemName },
-    { $inc: { "cart.$.quantity": 1 } },
+    { $inc: { "cart.$.quantity": amount } },
     function (err, updateResult) {
       if (err) {
-        printError(err)
+        printError(err);
       } else {
-        log({ updateResult })
+        // log({ updateResult });
       }
-    })
+    });
+  return incrementResult;
 }
 function addNewItemToCart(username, itemName, itemPrice) {
-  const newCartItem = { name: itemName, price: itemPrice, quantity: 1 }
-  log({ newCartItem })
+  const newCartItem = { name: itemName, price: itemPrice, quantity: 1 };
   cartModel.updateOne(
     { username: username },
     { $push: { cart: newCartItem } },
     { upsert: true },
     function (err, updateResult) {
       if (err) {
-        printError(err)
+        printError(err);
       } else {
-        log({ updateResult })
+        // log({ updateResult })
       }
     }
-  )
+  );
 }
+
+router.get("/increment/:name/:amount", async function (req, res) {
+  // console.log(req.params);
+  const { name, amount } = req.params;
+  await incrementItemQuantity(username, name, amount);
+  cartModel.findOne(
+    { username: username },
+    { cart: { $elemMatch: { name: name } } },
+    function (err, data) {
+      if (err) {
+        printError(err);
+      } else {
+        res.send({ quantity: data.cart[0].quantity });
+      }
+    });
+});
+
+router.get("/remove/:name", function (req, res) {
+  // remove one or all
+});
+
+
+router.get("/finalizeOrder", function (req, res) {
+  // move items to orderHistroy and remove all
+});
+
+router.get("/view", function (req, res) {
+  res.render('cart');
+});
+
+router.get("/getCart", function (req, res) {
+  cartModel.findOne({ username: username }, function (err, response) {
+    if (err) {
+      printError(err);
+    } else {
+      res.send(response.cart);
+    }
+  });
+});
 
 //-------------------- HELPER FUNCTIONS --------------------//
 
 function log(e) {
-  console.log(e)
+  console.log(e);
 }
 function printError(err) {
-  console.log(`Error: ${err}`)
+  console.log(`Error: ${err}`);
 }
 
 module.exports = router;
