45d76e10d04a669f9eb958006600339b35090236
Terence Grigoruk <terence.grigoruk@gmail.com>
2022-05-27 18:09:49 -0700

    Added game board creation function

--- public/js/game.js
+++ public/js/game.js
@@ -1,3 +1,36 @@
+const pokeapiUrl = "https://pokeapi.co/api/v2/";
+
+async function createBoard(rows, cols, numOfPokemon) {
+  let pokemonList = [];
+  let pokemonImage = "";
+  for (p = 0; p < numOfPokemon; p++) {
+    let randomPokemon = Math.trunc(Math.random() * 900);
+    // pokemonList.push(randomPokemon);
+    await $.ajax({
+      type: "GET",
+      url: `${pokeapiUrl}pokemon/${randomPokemon}`,
+      success: (pokemon) => {
+        pokemonImage = pokemon.sprites.other["official-artwork"].front_default;
+      },
+    });
+    pokemonList.push(pokemonImage);
+  }
+  console.log(pokemonList);
+  for (i = 0; i < rows * cols; i++) {
+    let thisPokemon = Math.trunc(Math.random() * numOfPokemon);
+    $("#game-grid").append(
+      `
+        <div class="game-card">
+          <img class="card-face" id="card-${i}" src="${pokemonList[thisPokemon]}" alt="" />
+          <img class="card-back" src="/images/ball.png" alt="" />
+        </div>
+      `
+    );
+  }
+  $("#game-grid").css(" grid-template-rows", `repeat(${cols}, 1fr)`);
+  $(".game-card").on("click", game);
+}
+
 firstCard = undefined;
 secondCard = undefined;
 carHasBeenFlipped = false;
@@ -10,41 +43,38 @@ matches = 0;
 // grab images randomly from API
 // timer for games 2 mins
 
-function setup() {
-  $(".game-card").on("click", function () {
-    $(this).toggleClass("flip");
+function game() {
+  $(this).toggleClass("flip");
 
-    if (!carHasBeenFlipped) {
-      // captured first card
-      firstCard = $(this).find(".card-face")[0];
-      //   console.log(firstCard);
-      cardHasBeenFlipped = true;
+  if (!carHasBeenFlipped) {
+    // captured first card
+    firstCard = $(this).find(".card-face")[0];
+    //   console.log(firstCard);
+    cardHasBeenFlipped = true;
+  } else {
+    secondCard = $(this).find(".card-face")[0];
+    cardHasBeenFlipped = false;
+    console.log({ firstCard, secondCard });
+
+    if (
+      $(`#${firstCard.id}`).attr("src") == $(`#${secondCard.id}`).attr("src")
+    ) {
+      console.log("Match!");
+      matches++;
+      $(`#${firstCard.id}`).parent().off("click");
+      $(`#${secondCard.id}`).parent().off("click");
     } else {
-      secondCard = $(this).find(".card-face")[0];
-      cardHasBeenFlipped = false;
-      console.log({ firstCard, secondCard });
-
-      if (
-        $(`#${firstCard.id}`).attr("src") == $(`#${secondCard.id}`).attr("src")
-      ) {
-        console.log("Match!");
-        matches++;
-        $(`#${firstCard.id}`).parent().off("click");
-        $(`#${secondCard.id}`).parent().off("click");
-      } else {
-        console.log("Not a atch!");
-        setTimeout(() => {
-          $(`#${firstCard.id}`).parent().removeClass(".flip");
-          $(`#${secondCard.id}`).parent().removeClass(".flip");
-        }, 2000);
-      }
+      console.log("Not a atch!");
+      setTimeout(() => {
+        $(`#${firstCard.id}`).parent().removeClass(".flip");
+        $(`#${secondCard.id}`).parent().removeClass(".flip");
+      }, 2000);
     }
-    // check for match
+  }
+}
 
-    // reset
-    // firstCard = undefined;
-    // secondCard = undefined;
-  });
+function setup() {
+  createBoard(4, 4, 5);
 }
 
 $(document).ready(setup);
