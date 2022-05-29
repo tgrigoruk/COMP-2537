43099153b7e2c681e0dbe59612c2592caa016bd3
Terence Grigoruk <terence.grigoruk@gmail.com>
2022-05-29 16:07:03 -0700

    Fixed bug in searching by name

--- public/js/search.js
+++ public/js/search.js
@@ -20,8 +20,7 @@ async function searchPokemons(searchUrl) {
 
 function searchPokemonByName(keypress) {
   if (keypress.keyCode == 13) {
-    pokemonNameTitleCase = $(this).val();
-    pokemonName = pokemonNameTitleCase.toLowerCase();
+    pokemonName = $(this).val().toLowerCase();
     if (/[^a-z]/i.test(pokemonName)) {
       alert("Invalid name entered - must contain only letters.");
     } else {
@@ -33,7 +32,7 @@ function searchPokemonByName(keypress) {
         },
         success: async (data) => {
           if (data.id) {
-            await addNameToTimeline(pokemonNameTitleCase);
+            logEvent(`Searched for ${pokemonName}`);
             window.location.href = `/pokemon/profile/${data.id}`;
           } else {
             alert("A pokemon by that name does not exist.");
@@ -42,21 +41,6 @@ function searchPokemonByName(keypress) {
       });
     }
   }
-}
-
-async function addNameToTimeline(pokemonName) {
-  pokemonName = pokemonName[0].toUpperCase() + pokemonName.slice(1);
-  await $.ajax({
-    url: `/timeline/insert`,
-    type: "POST",
-    data: {
-      text: `Searched for ${pokemonName}`,
-      time: time.toLocaleTimeString(),
-    },
-    success: (data) => {
-      loadEvents();
-    },
-  });
 }
 
 function setup() {
